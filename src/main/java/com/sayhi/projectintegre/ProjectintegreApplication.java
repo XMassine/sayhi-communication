9c0afa05b5293e3cf3ec3a429fab1596982e8116
massine <massineharcherras@gmail.com>
2025-01-14 13:34:17 +0200

    keycloack postgres and sec config

---
+++ src/main/java/com/sayhi/projectintegre/ProjectintegreApplication.java
@@ -0,0 +1,13 @@
+package com.sayhi.projectintegre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectintegreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectintegreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayhi/projectintegre/ProjectintegreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayhi.projectintegre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectintegreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
